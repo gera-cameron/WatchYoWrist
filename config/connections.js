45f2cb84d80c28b08c47c9d7bab8cd67e56a77e7
Kyle Brost <kjbrost@gmail.com>
2015-11-19 14:01:56 -0500

    modified credentials

--- config/connections.js
+++ config/connections.js
@@ -45,7 +45,8 @@ module.exports.connections = {
     host      : 'localhost',
     port      : 3306,
     user      : 'root',
-    database  : 'CISProject'
+    password  : 'K04b8244',
+    database  : 'OnlineStore'
   },
 
   /***************************************************************************
