3b9505f72ae3028d74e0f4d6311a989ee47e59c0
gera-cameron <gera.cameron@gmail.com>
2015-11-19 14:00:12 -0500

    AnOrder fix

--- api/models/AnOrder.js
+++ api/models/AnOrder.js
@@ -3,6 +3,23 @@
 *
 * @description :: TODO: You might write a short summary of how this model works and what it represents here.
 * @docs        :: http://sailsjs.org/#!documentation/models
+
+CREATE TABLE AnOrder (
+	id INT NOT NULL AUTO_INCREMENT,
+	date_bought timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	cur_product INT REFERENCES Product.id,
+	cur_user INT REFERENCES User.id,
+	paid BOOLEAN,
+	quantity INT CHECK (quantity <= (
+		SELECT quantity
+		FROM Product
+		WHERE product = Product.id)),
+	createdAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	updatedAt timestamp ON UPDATE CURRENT_TIMESTAMP,
+	PRIMARY KEY (id),
+	CHECK(quantity >= 1)
+);
+
 */
 
 module.exports = {
@@ -12,7 +29,8 @@ module.exports = {
   attributes: {
     id : {type:'INT'},
     date_bought : {type:'datetime'},
-    product : {type:'INT'},
+    cur_product : {type:'INT'},
+    cur_user : {type:'INT'},
     paid : {type:'boolean'},
     quantity : {type:'INT'},
   }
